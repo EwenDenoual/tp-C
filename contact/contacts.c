8d29d06be21c9f5a3654dd18e7796413cf954e71
dewen <ewen.denoual@ynov.com>
2026-06-11 11:21:34 +0200

    addmenu ok

---
+++ contact/contacts.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int choice = 1;
+
+    while (1) {
+        printf("\n\n\nMenu:\n");
+        printf("0. Exit\n");
+        printf("1. List contacts\n");
+        printf("2. Add contact\n");
+        printf("3. Remove contact\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 0:
+                return 0;
+            case 1:
+                printf("Listing contacts...\n");
+                // Code to list contacts
+                break;
+            case 2:
+                printf("Adding contact...\n");
+                // Code to add contact
+                break;
+            case 3:
+                printf("Removing contact...\n");
+                // Code to remove contact
+                break;
+        }
+    }
+}
